54b9abf5fc8787e09b26b6c2f1238670e520ab9b
tabloida <aydanartist@gmail.com>
2017-05-27 15:36:05 -0400

    fixed -ow

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var app = express();
 var http = require('http');
 app.server = http.createServer(app);
 var mzsi = require('mzsi');
-var ow = require('overwatch-js');
+var owjs = require('overwatch-js');
 var bodyParser = require('body-parser');
 var casual = require('casual');
 
